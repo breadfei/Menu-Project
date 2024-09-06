c05d81d48fd3367f00da67381d264e06e9d5ef92
breadfei <hartmanbrady1110@gmail.com>
2024-09-06 16:51:31 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from './models/menu-item';
+
+let totalPrice = 0;
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ChirpusPizza';
+  title = 'Bread Bank';
+  menuList: MenuItem[] = [
+    {name: "Baguette", category: "Long", price: 9.99, vegetarian: true, soldOut: false},
+    {name: "Brioche", category: "Bun", price: 8.99, vegetarian: true, soldOut: false},
+    {name: "Whole wheat", category: "Loaf", price: 4.99, vegetarian: true, soldOut: false},
+    {name: "Gluten free", category: "Loaf", price: 5.99, vegetarian: true, soldOut: true},
+    {name: "Texas toast", category: "Toast", price: 8.99, vegetarian: true, soldOut: false},
+    {name: "Gluten free", category: "Loaf", price: 5.99, vegetarian: true, soldOut: false},
+    {name: "Potato bread", category: "Potato", price: 10.99, vegetarian: true, soldOut: false}
+  ]
 }
